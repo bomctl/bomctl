72b948c4d4431fa2103e94939c0af5af30743de1
Jonathan Howard <jonathan.w.howard@lmco.com>
2024-04-14 16:29:03 -0500

    ci: configure tag signing
    Signed-off-by: Jonathan Howard <jonathan.w.howard@lmco.com>

---
+++ .github/scripts/generate-jwt.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly APP_ID="874590"
+
+now=$(date +%s)
+iat=$((now - 60))  # Issues 60 seconds in the past
+exp=$((now + 600)) # Expires 10 minutes in the future
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+  "typ":"JWT",
+  "alg":"RS256"
+}'
+
+# Header encode
+header=$(echo -n "${header_json}" | b64enc)
+
+payload_json='{
+  "iat":'"${iat}"',
+  "exp":'"${exp}"',
+  "iss":'"${APP_ID}"'
+}'
+
+# Payload encode
+payload=$(echo -n "${payload_json}" | b64enc)
+
+# Signature
+header_payload="${header}"."${payload}"
+signature=$(
+  openssl dgst -sha256 -sign <(echo -n "$GORELEASER_BOT_RSA_PRIVATE_KEY") \
+    <(echo -n "${header_payload}") | b64enc
+)
+
+# Create JWT
+JWT="${header_payload}"."${signature}"
+printf '%s\n' "$JWT"
---
+++ .github/scripts/tag-release.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &> /dev/null && pwd)"
+readonly SCRIPT_DIR
+
+# Install jq to parse JSON responses
+if ! command -v jq &> /dev/null; then
+  (apt-get update && apt-get install --yes jq) &> /dev/null
+fi
+
+jwt=$("$SCRIPT_DIR/generate-jwt.sh")
+
+# Get ID of GitHub App installation
+installation_id="$(curl --fail --silent --location --request GET \
+  --header "Authorization: Bearer $jwt" \
+  --url https://api.github.com/repos/bomctl/bomctl/installation | jq --raw-output .id)"
+
+# Get access token
+token="$(curl --fail --silent --location --request POST \
+  --header "Accept: application/vnd.github+json" \
+  --header "Authorization: Bearer $jwt" \
+  --header "X-GitHub-Api-Version: 2022-11-28" \
+  --url "https://api.github.com/app/installations/$installation_id/access_tokens" | jq --raw-output .token)"
+
+# Create a tag as reference
+curl --fail --silent --location --request POST \
+  --header "Accept: application/vnd.github+json" \
+  --header "Authorization: Bearer $token" \
+  --header "X-GitHub-Api-Version: 2022-11-28" \
+  --url https://api.github.com/repos/bomctl/bomctl/git/refs \
+  --data '{
+    "ref": "refs/tags/'"$NEXT_VERSION"'",
+    "sha": "'"$GITHUB_REF"'"
+  }'
