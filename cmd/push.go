b6843866b2d508a7663efbc7f09d85c050a5951b
Allen Shearin <allen.p.shearin@gmail.com>
2024-08-08 15:05:24 -0600

    feat: add push subcommand cli (#122)
    * feat: add push cli subcommand
    
    * Apply suggestions from code review
    
    Co-authored-by: jhoward-lm <140011346+jhoward-lm@users.noreply.github.com>
    Signed-off-by: Allen Shearin <allen.p.shearin@gmail.com>
    
    * update to handle multiple sbom ids with a single destination
    
    ---------
    
    Signed-off-by: Allen Shearin <allen.p.shearin@gmail.com>
    Co-authored-by: jhoward-lm <140011346+jhoward-lm@users.noreply.github.com>

---
+++ cmd/push.go
@@ -0,0 +1,56 @@
+// ------------------------------------------------------------------------
+// SPDX-FileCopyrightText: Copyright © 2024 bomctl a Series of LF Projects, LLC
+// SPDX-FileName: cmd/push.go
+// SPDX-FileType: SOURCE
+// SPDX-License-Identifier: Apache-2.0
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+package cmd
+
+import (
+	"github.com/protobom/protobom/pkg/formats"
+	"github.com/spf13/cobra"
+
+	"github.com/bomctl/bomctl/internal/pkg/options"
+	"github.com/bomctl/bomctl/internal/pkg/push"
+	"github.com/bomctl/bomctl/internal/pkg/utils"
+)
+
+const pushArgNum = 2
+
+func pushCmd() *cobra.Command {
+	opts := &push.Options{
+		Options: options.New(options.WithLogger(utils.NewLogger("push"))),
+	}
+
+	pushCmd := &cobra.Command{
+		Use:    "push [flags] SBOM_ID [SBOM_ID...] DEST_PATH",
+		Args:   cobra.MinimumNArgs(pushArgNum),
+		Short:  "Push stored SBOM file(s) to remote URL or filesystem",
+		Long:   "Push stored SBOM file(s) to remote URL or filesystem",
+		PreRun: preRun(opts.Options),
+		Run: func(_ *cobra.Command, args []string) {
+			if err := push.Push(args[:len(args)-1], args[len(args)-1], opts); err != nil {
+				opts.Logger.Fatal(err)
+			}
+		},
+	}
+
+	pushCmd.Flags().StringP("encoding", "e", formats.JSON, encodingHelp())
+	pushCmd.Flags().StringP("format", "f", formats.CDXFORMAT, formatHelp())
+	pushCmd.Flags().BoolVar(&opts.UseNetRC, "netrc", false, "Use .netrc file for authentication to remote hosts")
+	pushCmd.Flags().BoolVar(&opts.UseTree, "tree", false, "Recursively push all SBOMs in external reference tree")
+
+	return pushCmd
+}
