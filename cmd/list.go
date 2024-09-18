5fc1eca7e049378e3fd44668c7ab3769988975d6
Allen Shearin <allen.p.shearin@gmail.com>
2024-09-18 15:52:31 -0600

    address PR comments

--- cmd/list.go
+++ cmd/list.go
@@ -22,9 +22,10 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
-	"github.com/charmbracelet/lipgloss/table"
+	lgtable "github.com/charmbracelet/lipgloss/table"
 	"github.com/protobom/protobom/pkg/sbom"
 	"github.com/spf13/cobra"
 	"golang.org/x/term"
@@ -38,6 +39,11 @@ const (
 	columnIdxVersion
 	columnIdxNumNodes
 
+	columnNameID       = "ID"
+	columnNameAlias    = "Alias"
+	columnNameVersion  = "Version"
+	columnNameNumNodes = "# Nodes"
+
 	columnWidthID       = 47
 	columnWidthAlias    = 12
 	columnWidthVersion  = 9
@@ -45,14 +51,74 @@ const (
 
 	cellSideCount = 2
 
-	headerCount = 4
-
 	paddingHorizontal = 1
 	paddingVertical   = 0
 
 	rowHeaderIdx = 0
 	rowMaxHeight = 1
+
+	totalColumnCount = 4
 )
+
+type (
+	columnDefinition struct {
+		name  string
+		width int
+	}
+
+	rowData struct {
+		id, alias, version, numNodes string
+	}
+
+	table struct {
+		columns []columnDefinition
+		rows    []rowData
+	}
+)
+
+func (t *table) String() string {
+	tooWide := t.determineColumnWidths()
+
+	if tooWide {
+		return formatList(t)
+	}
+
+	return formatTable(t)
+}
+
+func newTable() *table {
+	cols := []columnDefinition{}
+
+	for c := range totalColumnCount {
+		name := ""
+		width := 0
+
+		switch c {
+		case columnIdxID:
+			name = columnNameID
+			width = columnWidthID
+		case columnIdxAlias:
+			name = columnNameAlias
+			width = columnWidthAlias
+		case columnIdxVersion:
+			name = columnNameVersion
+			width = columnWidthVersion
+		case columnIdxNumNodes:
+			name = columnNameNumNodes
+			width = columnWidthNumNodes
+		}
+
+		cols = append(cols, columnDefinition{
+			name:  name,
+			width: width,
+		})
+	}
+
+	return &table{
+		columns: cols,
+		rows:    []rowData{},
+	}
+}
 
 func getTermInfo() int {
 	width, _, err := term.GetSize(0)
@@ -63,42 +129,85 @@ func getTermInfo() int {
 	return width
 }
 
-func determineColumnWidths(rows ...[]string) (tooWide bool, colWidths []int) {
+func (t *table) getHeaders() []string {
+	headers := []string{}
+
+	for c := range totalColumnCount {
+		headers = append(headers, t.columns[c].name)
+	}
+
+	return headers
+}
+
+func (t *table) getRows() [][]string {
+	rows := [][]string{}
+
+	for _, row := range t.rows {
+		rows = append(rows, []string{row.id, row.alias, row.version, row.numNodes})
+	}
+
+	return rows
+}
+
+func (t *table) getTableWidth() int {
+	totalWidth := totalColumnCount * (paddingHorizontal * cellSideCount)
+
+	for c := range totalColumnCount {
+		totalWidth += t.columns[c].width
+	}
+
+	return totalWidth
+}
+
+func (t *table) determineColumnWidths() bool {
 	terminalWidth := getTermInfo()
-	idColumnWidth := columnWidthID
-	aliasColumnWidth := columnWidthAlias
+	padding := paddingHorizontal * cellSideCount
 
-	for _, row := range rows {
-		if idWidth := len(row[columnIdxID]); idWidth >= idColumnWidth {
-			idColumnWidth = idWidth + (paddingHorizontal * cellSideCount)
+	for _, row := range t.rows {
+		if idWidth := len(row.id); idWidth >= t.columns[columnIdxID].width {
+			t.columns[columnIdxID].width = idWidth + padding
 		}
 
-		if aliasWidth := len(row[columnIdxAlias]); aliasWidth >= aliasColumnWidth {
-			aliasColumnWidth = aliasWidth + (paddingHorizontal * cellSideCount)
+		if aliasWidth := len(row.alias); aliasWidth >= t.columns[columnIdxAlias].width {
+			t.columns[columnIdxAlias].width = aliasWidth + padding
+		}
+
+		if versionWidth := len(row.version); versionWidth >= columnIdxVersion {
+			t.columns[columnIdxVersion].width = versionWidth + padding
+		}
+
+		if numNodeWidth := len(row.numNodes); numNodeWidth >= t.columns[columnIdxNumNodes].width {
+			t.columns[columnIdxNumNodes].width = numNodeWidth + padding
 		}
 	}
 
-	totalWidth := (idColumnWidth + aliasColumnWidth + columnWidthVersion + columnWidthNumNodes)
-	totalWidth += headerCount * (paddingHorizontal * cellSideCount)
-
-	return (totalWidth > terminalWidth), []int{idColumnWidth, aliasColumnWidth, columnWidthVersion, columnWidthNumNodes}
+	return (t.getTableWidth() > terminalWidth)
 }
 
-func printList(rows ...[]string) {
-	for _, row := range rows {
-		fmt.Fprintf(os.Stdout, "ID: %s\n"+"Alias: %s\n"+"Version: %s\n"+"# Nodes: %s\n\n",
-			row[columnIdxID], row[columnIdxAlias], row[columnIdxVersion], row[columnIdxNumNodes])
+func formatList(outputTable *table) string {
+	output := ""
+	for _, row := range outputTable.rows {
+		output = strings.Join([]string{
+			output,
+			fmt.Sprintf("%-8s: %s", columnNameID, row.id),
+			fmt.Sprintf("%-8s: %s", columnNameAlias, row.alias),
+			fmt.Sprintf("%-8s: %s", columnNameVersion, row.version),
+			fmt.Sprintf("%-8s: %s", columnNameNumNodes, row.numNodes),
+			"",
+		}, "\n")
 	}
+
+	return output
 }
 
-func printTable(colWidths []int, rows ...[]string) {
+func formatTable(outputTable *table) string {
 	commonStyle := lipgloss.NewStyle().
 		Padding(paddingVertical, paddingHorizontal).
 		MaxHeight(rowMaxHeight)
 
-	fmt.Fprintf(os.Stdout, "\n%s\n\n", table.New().
-		Headers("ID", "Alias", "Version", "# Nodes").
-		Rows(rows...).
+	return lgtable.New().
+		Headers(outputTable.getHeaders()...).
+		Rows(outputTable.getRows()...).
 		BorderTop(false).
 		BorderBottom(false).
 		BorderLeft(false).
@@ -107,26 +216,26 @@ func printTable(colWidths []int, rows ...[]string) {
 		StyleFunc(func(row, col int) lipgloss.Style {
 			align := lipgloss.Center
 			width := 0
+			columns := outputTable.columns
 
 			switch col {
 			case columnIdxID:
-				width = colWidths[columnIdxID]
+				width = columns[columnIdxID].width
 
 				if row != rowHeaderIdx {
 					align = lipgloss.Left
 				}
 			case columnIdxAlias:
-				width = colWidths[columnIdxAlias]
+				width = columns[columnIdxAlias].width
 			case columnIdxVersion:
-				width = colWidths[columnIdxVersion]
+				width = columns[columnIdxVersion].width
 			case columnIdxNumNodes:
-				width = colWidths[columnIdxNumNodes]
+				width = columns[columnIdxNumNodes].width
 			}
 
 			return commonStyle.Width(width).AlignHorizontal(align)
 		}).
-		Render(),
-	)
+		Render()
 }
 
 func listCmd() *cobra.Command {
@@ -155,18 +264,12 @@ func listCmd() *cobra.Command {
 				}
 			}
 
-			rows := [][]string{}
+			listOutput := newTable()
 			for _, document := range documents {
-				rows = append(rows, getRow(document, backend))
+				listOutput.rows = append(listOutput.rows, getRow(document, backend))
 			}
 
-			tooWide, columnWidths := determineColumnWidths(rows...)
-
-			if tooWide {
-				printList(rows...)
-			} else {
-				printTable(columnWidths, rows...)
-			}
+			fmt.Fprintln(os.Stdout, listOutput.String())
 		},
 	}
 
@@ -176,7 +279,7 @@ func listCmd() *cobra.Command {
 	return listCmd
 }
 
-func getRow(doc *sbom.Document, backend *db.Backend) []string {
+func getRow(doc *sbom.Document, backend *db.Backend) rowData {
 	id := doc.GetMetadata().GetId()
 
 	alias, err := backend.GetDocumentUniqueAnnotation(doc.GetMetadata().GetId(), db.AliasAnnotation)
@@ -184,5 +287,10 @@ func getRow(doc *sbom.Document, backend *db.Backend) []string {
 		backend.Logger.Fatalf("failed to get alias: %v", err)
 	}
 
-	return []string{id, alias, doc.GetMetadata().GetVersion(), fmt.Sprint(len(doc.GetNodeList().GetNodes()))}
+	return rowData{
+		id:       id,
+		alias:    alias,
+		version:  doc.GetMetadata().GetVersion(),
+		numNodes: fmt.Sprint(len(doc.GetNodeList().GetNodes())),
+	}
 }
