9ce506eef87d0e7be8913b9062a97b315ae8d2fd
Jonathan Howard <jonathan.w.howard@lmco.com>
2024-09-19 12:22:38 -0500

    refactor: scoped test suites
    Signed-off-by: Jonathan Howard <jonathan.w.howard@lmco.com>

--- internal/pkg/client/git/client_test.go
+++ internal/pkg/client/git/client_test.go
@@ -20,194 +20,20 @@
 package git_test
 
 import (
-	"context"
-	"fmt"
-	"net/http/cgi"
-	"net/http/httptest"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"testing"
-	"time"
 
-	"github.com/go-git/go-billy/v5/osfs"
-	gogit "github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/cache"
-	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/plumbing/serverinfo"
-	"github.com/go-git/go-git/v5/storage/filesystem"
-	"github.com/protobom/protobom/pkg/sbom"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/bomctl/bomctl/internal/pkg/client/git"
-	"github.com/bomctl/bomctl/internal/pkg/db"
-	"github.com/bomctl/bomctl/internal/pkg/options"
 	"github.com/bomctl/bomctl/internal/pkg/url"
 )
 
-type gitSuite struct {
+type gitClientSuite struct {
 	suite.Suite
-	tmpDir  string
-	opts    *options.Options
-	backend *db.Backend
-	gc      *git.Client
-	server  *httptest.Server
-	docs    []*sbom.Document
 }
 
-func (gs *gitSuite) setupGitServer() {
-	gs.T().Helper()
-
-	// Create server root and test repository directories.
-	serverRoot := filepath.Join(gs.tmpDir, "git-test-server")
-	repoDir := filepath.Join(serverRoot, "test", "repo.git")
-	gs.Require().NoError(os.MkdirAll(repoDir, os.ModePerm))
-
-	// Create storage for test Git server repository.
-	repoFS := osfs.New(repoDir)
-	storer := filesystem.NewStorage(repoFS, cache.NewObjectLRUDefault())
-
-	// Initialize test Git server repository.
-	repo, err := gogit.InitWithOptions(storer, repoFS, gogit.InitOptions{DefaultBranch: plumbing.Main})
-	gs.Require().NoError(err)
-
-	worktree, err := repo.Worktree()
-	gs.Require().NoError(err)
-
-	// Create initial commit and pack Git objects.
-	_, err = worktree.Commit("Initial commit", &gogit.CommitOptions{
-		AllowEmptyCommits: true,
-		Author: &object.Signature{
-			Name:  "bomctl-unit-test",
-			Email: "bomctl-unit-test@users.noreply.github.com",
-			When:  time.Now(),
-		},
-	})
-	gs.Require().NoError(err)
-	gs.Require().NoError(repo.Storer.PackRefs())
-	gs.Require().NoError(serverinfo.UpdateServerInfo(storer, repoFS))
-
-	// Update test Git server repository config to bare and unset worktree.
-	// This is to allow clients to update the server repository's main branch.
-	repoConfig, err := repo.Config()
-	gs.Require().NoError(err)
-
-	repoConfig.Core.IsBare = true
-	repoConfig.Core.Worktree = ""
-
-	gs.Require().NoError(repo.SetConfig(repoConfig), "Failed to set Git repo config")
-
-	// Get path to git executable.
-	gitPath, err := exec.LookPath("git")
-	gs.Require().NoError(err, "Unable to find git executable")
-
-	// Create CGI handler to handle Git smart protocol requests.
-	gitHandler := &cgi.Handler{
-		Path: gitPath,
-		Args: []string{
-			"-c", "http.getanyfile",
-			"-c", "http.receivepack",
-			"-c", "http.uploadpack",
-			"http-backend",
-		},
-		Env: []string{fmt.Sprintf("GIT_PROJECT_ROOT=%s", serverRoot), "GIT_HTTP_EXPORT_ALL=true"},
-	}
-
-	// Start the test server.
-	gs.server = httptest.NewServer(gitHandler)
-}
-
-func (gs *gitSuite) BeforeTest(_suiteName, _testName string) {
-	var err error
-
-	gs.tmpDir, err = os.MkdirTemp("", "git-client-test")
-	gs.Require().NoErrorf(err, "Failed to create temporary directory: %v", err)
-
-	gs.setupGitServer()
-
-	pushOpts := &options.PushOptions{Options: gs.opts}
-	gs.Require().NoError(
-		gs.gc.PreparePush(
-			fmt.Sprintf("%s/test/repo.git@main#path/to/sbom.cdx", gs.server.URL),
-			pushOpts,
-		),
-	)
-
-	repoConfig, err := gs.gc.Repo().Config()
-	gs.Require().NoError(err)
-
-	repoConfig.Author.Name = "bomctl-unit-test"
-	repoConfig.Author.Email = "bomctl-unit-test@users.noreply.github.com"
-
-	gs.Require().NoError(gs.gc.Repo().SetConfig(repoConfig), "Failed to set Git repo config")
-}
-
-func (gs *gitSuite) AfterTest(_suiteName, _testName string) {
-	gs.server.Close()
-
-	defer os.RemoveAll(gs.tmpDir)
-}
-
-func (gs *gitSuite) SetupSuite() {
-	backend, err := db.NewBackend(db.WithDatabaseFile(db.DatabaseFile))
-	if err != nil {
-		gs.T().Fatalf("%v", err)
-	}
-
-	gs.backend = backend
-
-	testdataDir := filepath.Join("..", "..", "db", "testdata")
-
-	sboms, err := os.ReadDir(testdataDir)
-	if err != nil {
-		gs.T().Fatalf("%v", err)
-	}
-
-	for sbomIdx := range sboms {
-		sbomData, err := os.ReadFile(filepath.Join(testdataDir, sboms[sbomIdx].Name()))
-		if err != nil {
-			gs.T().Fatalf("%v", err)
-		}
-
-		doc, err := gs.backend.AddDocument(sbomData)
-		if err != nil {
-			gs.FailNow("failed storing document", "err", err)
-		}
-
-		gs.docs = append(gs.docs, doc)
-	}
-
-	gs.opts = options.New().WithCacheDir(viper.GetString("cache_dir"))
-	gs.opts = gs.opts.WithContext(context.WithValue(context.Background(), db.BackendKey{}, gs.backend))
-	gs.gc = &git.Client{}
-}
-
-func (gs *gitSuite) TearDownSuite() {
-	gs.server.Close()
-
-	err := os.RemoveAll(gs.tmpDir)
-	if err != nil {
-		gs.T().Fatalf("Error removing repo file %s", db.DatabaseFile)
-	}
-
-	gs.backend.CloseClient()
-
-	if _, err := os.Stat(db.DatabaseFile); err == nil {
-		if err := os.Remove(db.DatabaseFile); err != nil {
-			gs.T().Fatalf("Error removing database file %s", db.DatabaseFile)
-		}
-	}
-}
-
-func TestGitSuite(t *testing.T) {
-	t.Parallel()
-	suite.Run(t, new(gitSuite))
-}
-
-func (gs *gitSuite) TestParse() {
-	gs.T().Parallel()
+func (gcs *gitClientSuite) TestClient_Parse() {
+	client := &git.Client{}
 
 	for _, data := range []struct {
 		expected *url.ParsedURL
@@ -428,8 +254,14 @@ func (gs *gitSuite) TestParse() {
 			expected: nil,
 		},
 	} {
-		actual := gs.gc.Parse(data.url)
-
-		gs.Equal(data.expected, actual)
+		gcs.Run(data.name, func() {
+			actual := client.Parse(data.url)
+			gcs.Require().Equal(data.expected, actual, data.url)
+		})
 	}
+}
+
+func TestGitClientSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(gitClientSuite))
 }
