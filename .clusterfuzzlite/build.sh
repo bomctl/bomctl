d3bcc1d70b3f1c8880ce695524dd7b0a070207e8
lallevato-lm <luke.d.allevato@lmco.com>
2024-11-25 15:52:39 -0700

    chore: dockerfile headers
    Signed-off-by: lallevato-lm <luke.d.allevato@lmco.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,27 +1,6 @@
 #!/bin/bash -eu
-# -----------------------------------------------------------------------------
-# SPDX-FileCopyrightText: Copyright © 2024 bomctl a Series of LF Projects, LLC
-# SPDX-FileName: .clusterfuzzlite/build.sh
-# SPDX-FileType: SOURCE
-# SPDX-License-Identifier: Apache-2.0
-# -----------------------------------------------------------------------------
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# -----------------------------------------------------------------------------
 
-# Download dependency and let the Go package manager do all the work
-# go mod tidy
+# make clean
+# go build
 
-make clean
-go build
-
-compile_native_go_fuzzer github.com/bomctl/bomctl/cmd/fuzz_test.go
+# compile_native_go_fuzzer github.com/bomctl/bomctl/cmd/fuzz_test.go
