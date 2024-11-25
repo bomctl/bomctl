fecee3d8606a7d3e5e9d16c4a424719594c8d42f
lallevato-lm <luke.d.allevato@lmco.com>
2024-11-25 16:00:46 -0700

    chore: dockerfile name
    Signed-off-by: lallevato-lm <luke.d.allevato@lmco.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -eu
 
-# make clean
-# go build
+# build project
+# e.g.
+# ./autogen.sh
+# ./configure
+# make -j$(nproc) all
 
-# compile_native_go_fuzzer github.com/bomctl/bomctl/cmd/fuzz_test.go
+# build fuzzers
+# e.g.
+# $CXX $CXXFLAGS -std=c++11 -Iinclude \
+#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
+#     $LIB_FUZZING_ENGINE /path/to/library.a
