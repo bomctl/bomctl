132347ae260ae46376ea84afc44cfd68ee0525f6
lallevato-lm <luke.d.allevato@lmco.com>
2024-11-26 09:05:52 -0700

    chore: docker build
    Signed-off-by: lallevato-lm <luke.d.allevato@lmco.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,15 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# -----------------------------------------------------------------------------
+# SPDX-FileCopyrightText: Copyright © 2024 bomctl a Series of LF Projects, LLC
+# SPDX-FileName: .clusterfuzzlite/build.sh
+# SPDX-FileType: SOURCE
+# SPDX-License-Identifier: Apache-2.0
+# -----------------------------------------------------------------------------
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------------
 
-# build project
-# e.g.
-# ./autogen.sh
-# ./configure
-# make -j$(nproc) all
+set -euo pipefail
 
-# build fuzzers
-# e.g.
-# $CXX $CXXFLAGS -std=c++11 -Iinclude \
-#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
-#     $LIB_FUZZING_ENGINE /path/to/library.a
-go build
+# go get github.com/bomctl/bomctl/internal/pkg
+go build -o bomctl .
+
 compile_go_fuzzer github.com/bomctl/bomctl/internal/pkg FuzzFetch Fuzz
