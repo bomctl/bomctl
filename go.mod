95c986a868c4fbae3ae5d0ab8c5e9c3ae208b182
Philippe <philippe.a.aviles@lmco.com>
2024-11-26 19:19:17 -0400

    feat: add gitlab fetch
    Signed-off-by: Philippe <philippe.a.aviles@lmco.com>

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
+	github.com/xanzy/go-gitlab v0.114.1-0.20241121191659-8186bd9e436a
 	golang.org/x/term v0.25.0
 	google.golang.org/protobuf v1.35.1
 	oras.land/oras-go/v2 v2.5.0
@@ -50,6 +51,10 @@ require (
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-openapi/inflect v0.21.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.22.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -86,10 +91,13 @@ require (
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.26.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/pkg/client/gitlab/fetch.go
@@ -0,0 +1,192 @@
+// -----------------------------------------------------------------------------
+// SPDX-FileCopyrightText: Copyright © 2024 bomctl a Series of LF Projects, LLC
+// SPDX-FileName: internal/pkg/client/gitlab/fetch.go
+// SPDX-FileType: SOURCE
+// SPDX-License-Identifier: Apache-2.0
+// -----------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// -----------------------------------------------------------------------------
+
+package gitlab
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/xanzy/go-gitlab"
+
+	"github.com/bomctl/bomctl/internal/pkg/options"
+)
+
+type dependencyListExportSession struct {
+	Client *gitlab.Client
+	Export *gitlab.DependencyListExport
+}
+
+var (
+	errInvalidGitLabURL = errors.New("invalid URL for GitLab fetching")
+	errFailedWebRequest = errors.New("web request failed")
+	errForbiddenAccess  = errors.New("you don't have permission to read the dependency list")
+)
+
+func newDependencyListExportSession(baseURL, gitLabToken string) (*dependencyListExportSession, error) {
+	gitlabClient, err := gitlab.NewClient(gitLabToken, gitlab.WithBaseURL(baseURL))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client: %w", err)
+	}
+
+	newSession := &dependencyListExportSession{
+		Client: gitlabClient,
+		Export: nil,
+	}
+
+	return newSession, nil
+}
+
+func validateHTTPStatusCode(statusCode int) error {
+	if statusCode < http.StatusOK || http.StatusMultipleChoices <= statusCode {
+		return fmt.Errorf("%w. HTTP status code: %d", errFailedWebRequest, statusCode)
+	}
+
+	return nil
+}
+
+func (session *dependencyListExportSession) createExport(projectName, branchName string) error {
+	project, response, err := session.Client.Projects.GetProject(projectName, nil)
+	if err != nil {
+		return fmt.Errorf("failed to get project info: %w", err)
+	}
+
+	if err := validateHTTPStatusCode(response.StatusCode); err != nil {
+		return err
+	}
+
+	branch, response, err := session.Client.Branches.GetBranch(project.ID, branchName)
+	if err != nil {
+		return fmt.Errorf("failed to get branch: %w", err)
+	}
+
+	if err := validateHTTPStatusCode(response.StatusCode); err != nil {
+		return err
+	}
+
+	commit, response, err := session.Client.Commits.GetCommit(project.ID, branch.Commit.ID, nil)
+	if err != nil {
+		return fmt.Errorf("failed to get commit info: %w", err)
+	}
+
+	if err := validateHTTPStatusCode(response.StatusCode); err != nil {
+		return err
+	}
+
+	// NOTE:
+	// If an authenticated user does not have permission to read_dependency,
+	// this request returns a 403 Forbidden status code.
+	export, response, err := session.Client.DependencyListExport.CreateDependencyListExport(commit.LastPipeline.ID, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create dependency list export: %w", err)
+	}
+
+	if err := validateHTTPStatusCode(response.StatusCode); err != nil {
+		return err
+	}
+
+	if response.StatusCode == http.StatusForbidden {
+		return fmt.Errorf("%w", errForbiddenAccess)
+	}
+
+	session.Export = export
+
+	return nil
+}
+
+func (session *dependencyListExportSession) pollExportUntilFinished() error {
+	const waitSeconds = 2
+	for !session.Export.HasFinished {
+		time.Sleep(waitSeconds * time.Second)
+
+		updatedExport, response, err := session.Client.DependencyListExport.GetDependencyListExport(session.Export.ID)
+		if err != nil {
+			return fmt.Errorf("failed to get dependency list export: %w", err)
+		}
+
+		if err := validateHTTPStatusCode(response.StatusCode); err != nil {
+			return err
+		}
+
+		session.Export = updatedExport
+	}
+
+	return nil
+}
+
+func (session *dependencyListExportSession) downloadExport() ([]byte, error) {
+	sbomReader, response, err := session.Client.DependencyListExport.DownloadDependencyListExport(session.Export.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to download dependency list: %w", err)
+	}
+
+	if err := validateHTTPStatusCode(response.StatusCode); err != nil {
+		return nil, err
+	}
+
+	var sbomData []byte
+
+	_, err = sbomReader.Read(sbomData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read SBOM data: %w", err)
+	}
+
+	return sbomData, nil
+}
+
+func (client *Client) Fetch(fetchURL string, _ *options.FetchOptions) ([]byte, error) {
+	url := client.Parse(fetchURL)
+	if url == nil {
+		return nil, fmt.Errorf("%w: %s", errInvalidGitLabURL, fetchURL)
+	}
+
+	domain := url.Hostname
+	if url.Port != "" {
+		domain = fmt.Sprintf("%s:%s", domain, url.Port)
+	}
+
+	baseURL := fmt.Sprintf("https://%s/api/v4", domain)
+	projectName := url.Path
+	branchName := url.Fragment
+
+	gitLabToken := os.Getenv("GITLAB_FETCH_TOKEN")
+
+	session, err := newDependencyListExportSession(baseURL, gitLabToken)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	if err := session.createExport(projectName, branchName); err != nil {
+		return nil, err
+	}
+
+	if err := session.pollExportUntilFinished(); err != nil {
+		return nil, err
+	}
+
+	sbomData, err := session.downloadExport()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	return sbomData, nil
+}
