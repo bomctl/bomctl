f2df9eb9614a9121769d3a4e3f6f63e36c0b1fcc
lallevato-lm <luke.d.allevato@lmco.com>
2024-12-02 13:01:44 -0700

    chore: export options context
    Signed-off-by: lallevato-lm <luke.d.allevato@lmco.com>

--- internal/pkg/export/export_fuzz.go
+++ internal/pkg/export/export_fuzz.go
@@ -20,7 +20,10 @@
 package export
 
 import (
+	"context"
 	"testing"
+
+	"github.com/bomctl/bomctl/internal/pkg/options"
 )
 
 const testID = "urn:uuid:f360ad8b-dc41-4256-afed-337a04dff5db"
@@ -29,7 +32,11 @@ func FuzzExport(f *testing.F) {
 	f.Add([]byte(testID))
 
 	f.Fuzz(func(t *testing.T, id string) {
-		err := Export(id, nil)
+		ctx := context.Background()
+		opts := options.Options{}
+		opts.WithContext(ctx)
+		eOpts := options.ExportOptions{Options: &opts}
+		err := Export(id, &eOpts)
 
 		if err == nil {
 			t.Errorf("%s", err)
